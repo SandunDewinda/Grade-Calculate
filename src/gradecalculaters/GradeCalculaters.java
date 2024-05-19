14bffed210ace5452e9ebbda0f537e2c239f06fc
SandunDewinda <84126251+SandunDewinda@users.noreply.github.com>
2024-05-19 09:42:28 +0530

    First Commit

---
+++ src/gradecalculaters/GradeCalculaters.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gradecalculaters;
+
+public class GradeCalculaters {
+
+    public static void main(String[] args) {
+       
+        GradeStatus obj = new GradeStatus();
+        obj.setVisible(true);
+    }
+    
+}
